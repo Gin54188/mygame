5e432ac69afb671b293b026f12fa6b511c52bfa8
yesho <yesho@10.127.50.101>
2026-01-09 09:54:10 -0500

    Game files

--- src/character/Enemy.java
+++ src/character/Enemy.java
@@ -2,17 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package mygame;
+package character;
 
 /**
  *
  * @author yesho
  */
-public class Player {
-    private String name;          // instance variable
-    private int clueCount = 0;
+public class Enemy {
     
-    public Player(String name) {
-    this.name = name;
-
 }
